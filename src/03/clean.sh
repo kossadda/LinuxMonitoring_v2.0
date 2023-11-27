e4800f169a0a38164b2ec9dcf3f6d8a51eae659f
Your Name <you@example.com>
2023-11-27 13:25:18 +0500

    3 taskk

--- src/03/clean.sh
+++ src/03/clean.sh
@@ -22,4 +22,28 @@ timedate_clean() {
     echo "Удаление завершено." >> "$log_file"
 }
 
-timedate_clean
+
+log_clean() {
+    local files=""
+    local presence=0
+    if [ -e "report.log" ]; then
+        presence=1
+    elif [ -e "../02/report.log" ]; then
+        presence=2
+    fi
+    
+    if [ $presence -eq 1 ]; then
+        files=$(grep "_[0-9]\{6\}$" report.log | sed 's/Date [0-9.]\+ created folder: //')
+    elif [ $presence -eq 2 ]; then
+        files=$(grep "_[0-9]\{6\}$" ../02/report.log | sed 's/Date [0-9.]\+ created folder: //')
+    fi
+
+    if [ $presence -ne 0 ]; then
+        echo "$files"
+    else
+        echo "The report.log file is missing in directory 02 or 03"
+    fi
+}
+
+# timedate_clean
+log_clean
