102a6c064888e444ea324ccb8b9c922ced974772
kossadda <gabilov1997@gmail.com>
2024-03-09 01:11:03 +0500

    feat: start refactor of 1 task

---
+++ src/01/generate_name.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+gen_name() {
+  local addition_count=$((RANDOM % (75 - 4) + 4))
+  local name=$1
+  local random_index
+
+  for ((i = 0; i < ${addition_count}; i++)); do
+    random_index=$((RANDOM % (${#name} - 1) + 1))
+    name=${name:0:random_index}${name:random_index:1}${name:random_index:${#name}}
+  done
+
+  echo ${name}
+}
+
+filename() {
+  local filename=$(gen_name ${FILE_CHARS})
+
+  echo "${filename}_${DATE}.${FILE_EXTENSION}"
+}
+
+foldername() {
+  local foldername=$(gen_name ${FOLDER_CHARS})
+
+  echo "${foldername}_${DATE}"
+}
