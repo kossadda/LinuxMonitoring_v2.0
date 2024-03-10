b2c6b5255e25f0abda48e0a8d5fcf3b56eb4acc4
kossadda <gabilov1997@gmail.com>
2024-03-10 19:12:37 +0500

    feat: 01 change report output

--- src/01/dialog.sh
+++ src/01/dialog.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-generate_status() {
-  echo -ne "\rIn progress... Generated ${YELLOW}${FOLDERS}${RESET} folders and ${YELLOW}${FILES}${RESET} files"
-}
-
-report_file_create() {
-  echo "Date ${REPORT_DATE} created ${INPUT_SIZE} sized file: ${folder_in_depth_dir}/${file_name}" >> ${LOG_PATH}
-}
-
-report_folder_create() {
-  echo "Date ${REPORT_DATE} created folder: ${folder_in_depth_dir}/${folder_name}" >> ${LOG_PATH}
-}
-
 input_information() {
   echo -en "${YELLOW}"
   echo " __________________________________________________________________"
@@ -35,7 +23,6 @@ input_information() {
   echo -e "${RESET}"
 }
 
-
 output_result() {
   readonly END_TIME=$(date +%s)
   readonly EXECUTE_TIME=$((END_TIME - START_TIME))
@@ -49,10 +36,10 @@ output_result() {
 
   echo "Script started at ${REPORT_START_TIME}"
   echo "Script finised at ${REPORT_END_TIME}"
-  (echo "Script started at ${REPORT_START_TIME}")  >> ${LOG_PATH}
-  (echo "Script finised at ${REPORT_END_TIME}") >> ${LOG_PATH}
   bold "\nGenerated ${YELLOW}${FOLDERS}${RESET} ${BOLD}folders and ${YELLOW}${FILES}${RESET} ${BOLD}files for ${YELLOW}${EXECUTE_TIME}${RESET} ${BOLD}seconds\n"
   echo -e "${BOLD}File ${YELLOW}${LOG_PATH}${RESET} ${BOLD}contains information about the generation${RESET}"
+
+  report_results
 
   echo -e "${YELLOW}"
   echo " __________________________________________________________________"
@@ -60,4 +47,22 @@ output_result() {
   echo "|                             COMPLETE                             |"
   echo "|__________________________________________________________________|"
   echo -e "${RESET}"
+}
+
+report_results() {
+  sed -i "1s|^|$(echo -e "Script finised at ${REPORT_END_TIME}")\n\n\n|" ${LOG_PATH}
+  sed -i "1s|^|$(echo -e "Script started at ${REPORT_START_TIME}")\n|" ${LOG_PATH}
+  sed -i "1s|^|$(echo -e "Generated ${FOLDERS} folders and ${FILES} files for ${EXECUTE_TIME} seconds")\n|" ${LOG_PATH}
+}
+
+generate_status() {
+  echo -ne "\rIn progress... Generated ${YELLOW}${FOLDERS}${RESET} folders and ${YELLOW}${FILES}${RESET} files"
+}
+
+report_file_create() {
+  echo "Date ${REPORT_DATE} created ${INPUT_SIZE} sized file: ${folder_in_depth_dir}/${file_name}" >> ${LOG_PATH}
+}
+
+report_folder_create() {
+  echo "Date ${REPORT_DATE} created folder: ${folder_in_depth_dir}/${folder_name}" >> ${LOG_PATH}
 }
